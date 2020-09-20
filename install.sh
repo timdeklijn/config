28b6d5662a53ea9d430ac11e78981799fdf53a67
Tim de Klijn <timdeklijn@gmail.com>
2020-09-20 16:44:29 +0200

    added simple install script

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
 # Install Script
+
+# Create symlink to init.vim
+echo "-- Replacing nvim config file"
+rm ~/.config/nvim/init.vim
+ln -s vim.vim ~/.config/nvim/init.vim
+
+# Create symlink to .zshrc
+echo "-- Replacing zshrc config file"
+rm ~/.zshrc
+ln -s zshrc ~/.zshrc
+
+# Create symlink to .tmux.conf
+echo "-- Replacing tmux config file"
+rm ~/.tmux.conf
+ln -s tmux.conf ~/.tmux.conf
