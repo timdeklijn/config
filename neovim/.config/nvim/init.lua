b830d6cfaca8bd489c637ee842787f4f66406bc2
Tim de Klijn <timdeklijn@gmail.com>
2023-01-23 14:48:12 +0100

    transparent background

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -131,15 +131,15 @@ vim.o.background = "dark"
 require("tokyonight").setup({
   style = "night",
   light_style = "day",
-  transparent = false,
+  transparent = true,
   terminal_colors = true,
   styles = {
     comments = { italic = false },
     keywords = { bold = true },
     functions = { bold = true },
     variables = { bold = false },
-    sidebars = "dark",
-    floats = "dark",
+    sidebars = "transparent",
+    floats = "transparent",
   },
   day_brightness = 0.3,
   hide_inactive_statusline = true,
@@ -203,15 +203,6 @@ require('gitsigns').setup {
 
 -- [[ Configure Telescope ]]
 require('telescope').setup {
-  pickers = {
-    find_files = {
-      hidden = true,
-    },
-    buffers = {
-      ignore_current_buffer = true,
-      sort_lastused = true,
-    },
-  },
   defaults = {
     file_ignore_patterns = { '.git' },
     mappings = {
@@ -221,26 +212,58 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    buffers = {
+      ignore_current_buffer = true,
+      sort_lastused = true,
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>?',
+  require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' }
+)
 
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader><space>',
+  require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' }
+)
+
+vim.keymap.set('n', '<leader>/',
+  function()
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      winblend = 10,
+      previewer = false,
+    })
+  end,
+  { desc = '[/] Fuzzily search in current buffer]' }
+)
+
+vim.keymap.set('n', '<leader>f',
+  require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' }
+)
+
+vim.keymap.set('n', '<leader>sh',
+  require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' }
+)
+
+vim.keymap.set('n', '<leader>sw',
+  require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' }
+)
+
+vim.keymap.set('n', '<leader>sg',
+  require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' }
+)
+
+vim.keymap.set('n', '<leader>sd',
+  require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' }
+)
 
 -- [[ Configure Treesitter ]]
 require('nvim-treesitter.configs').setup {
