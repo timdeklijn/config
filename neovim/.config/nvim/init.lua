114ca10b68ca39dc67c4013e106426bc1e15d3e1
Tim de Klijn <tim@deklijn.com>
2023-10-23 14:08:41 +0200

    (neovim): Moonfly

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -122,7 +122,7 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.g.moonflyCursorColor = true
-      vim.g.moonflyItalics = false
+      vim.g.moonflyItalics = true
       vim.g.moonflyNormalFloat = true
       vim.g.moonflyUndercurls = false
       vim.g.moonflyUnderlineMatchParen = true
@@ -559,6 +559,22 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.tfstate, *.tfstate.backup' },
   group = terraform_group,
 })
+
+-- Auto format imports on save (used for Golang)
+function OrganizeImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
