a705de18ad18c531a47dc1ea3eb91d61cd41dfd3
Tim de Klijn <tim@deklijn.com>
2023-11-23 10:00:53 +0100

    feat(all): nord + Iosevka

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -141,34 +141,18 @@ require('lazy').setup({
     },
   },
 
-  { "ellisonleao/gruvbox.nvim",
-    priority = 1000 ,
-    opts = {
-      terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = false,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-    },
+  {
+    'shaunsingh/nord.nvim',
+    priority = 1000,
     config = function()
-      vim.cmd[[ colorscheme gruvbox ]]
+      -- Example config in lua
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = true
+      vim.g.nord_disable_background = false
+      vim.g.nord_italic = true
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = true
+      vim.cmd[[ colorscheme nord ]]
     end
   },
 
