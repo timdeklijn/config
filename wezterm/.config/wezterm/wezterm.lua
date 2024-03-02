a860872d8c09087005e34364ef95225ef6fe7514
Tim de Klijn <timdeklijn@gmail.com>
2024-03-02 14:54:32 +0100

    feat(Wezterm): Font + sound

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,16 +2,20 @@ local wezterm = require("wezterm")
 local config = {}
 
 -- font config
-config.font = wezterm.font "JetBrainsMono Nerd Font"
-config.font_size = 20.0
-config.line_height = 1.3
+config.font = wezterm.font "CaskaydiaCove Nerd Font Mono"
+config.font_size = 22.0
+config.line_height = 1.5
+
+
+-- Wezterm settings
+config.audible_bel = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   bottom = 0,
   top= 0,
 }
 
--- Colors
+-- Color settings
 config.color_scheme = "nord"
 
 return config
