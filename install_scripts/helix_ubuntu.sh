7601716e96c9e61c3dddef779b7d582eef286ceb
Tim de Klijn <timdeklijn@gmail.com>
2024-04-02 14:11:29 +0200

    update helix install for ubuntu

--- install_scripts/helix_ubuntu.sh
+++ install_scripts/helix_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curk -LO https://github.com/helix-editor/helix/releases/download/24.03/helix-24.03-x86_64.AppImage
+curl -LO https://github.com/helix-editor/helix/releases/download/24.03/helix-24.03-x86_64.AppImage
 chmod +x helix-*.AppImage # change permission for executable mode
-sudo mv helix-*.AppImage ~/bin/heliX
+sudo mv helix-*.AppImage ~/bin/helix
