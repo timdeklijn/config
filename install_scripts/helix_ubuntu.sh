4aed0dbea23501562ab05fa89d47d89cdabdeed7
Tim de Klijn <timdeklijn@gmail.com>
2024-04-08 09:58:49 +0200

    rename `helix` to `hx` on PopOS

--- install_scripts/helix_ubuntu.sh
+++ install_scripts/helix_ubuntu.sh
@@ -2,4 +2,4 @@
 
 curl -LO https://github.com/helix-editor/helix/releases/download/24.03/helix-24.03-x86_64.AppImage
 chmod +x helix-*.AppImage # change permission for executable mode
-sudo mv helix-*.AppImage ~/bin/helix
+sudo mv helix-*.AppImage ~/bin/hx
